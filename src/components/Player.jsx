83637010ee85c433f8dee22eaa93d683d2c95f71
tenextractor <139619642+tenextractor@users.noreply.github.com>
2024-08-28 18:01:58 +0530

    Multiple music support

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,25 +1,42 @@
 import React, { useRef, useState } from 'react';
 
 const Player = () => {
-    const musicRef = useRef(new Audio("/music/AdoValue.mp3"));
+    const musicNames = ["Home Stretch - Gunnar Olsen.opus", "Trancer - Gunnar Olsen.opus", "Tremsz - Gunnar Olsen.opus"]
+    //const musicRef = useRef(new Audio("/music/AdoValue.mp3"));
+    const [nowPlaying, setNowPlaying] = useState(0);
     const [isPlaying, setIsPlaying] = useState(false);
+    //const [musicRef, setMusicRef] = useState(new Audio("/music/" + musicNames[nowPlaying]));
+    const [musicRefs, setMusicRefs] = useState(musicNames.map((name) => {return new Audio("/music/" + name)}));
+
+    const playNewMusic = (index) => {
+        musicRefs[nowPlaying].pause()
+        setNowPlaying(index);
+        musicRefs[index].play();
+        setIsPlaying(true);
+    }
 
     const toggleMusic = () => {
         if (isPlaying) {
-          musicRef.current.pause();
+          musicRefs[nowPlaying].pause();
         } else {
-          musicRef.current.play();
+          musicRefs[nowPlaying].play();
         }
         setIsPlaying(!isPlaying);
     };
     
     const resetMusic = () => {
-        musicRef.current.currentTime = 0;
+        musicRefs[nowPlaying].currentTime = 0;
         setIsPlaying(false);
-        musicRef.current.pause();
+        musicRefs[nowPlaying].pause();
     };
   return (
     <>
+        <ul>
+            {musicNames.map((name, index) => (
+                <li key={index} onClick={() => {playNewMusic(index)}} className={`${index == nowPlaying && "bg-gray-300 text-blue-500"}`}>{name}</li>
+            ))}
+        </ul>
+
         <button onClick={toggleMusic} className={`${
             isPlaying ? "bg-red-500 hover:bg-red-700" : "bg-green-500 hover:bg-green-700"
         } text-white font-bold py-2 px-4 rounded shadow-lg transition duration-300 transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500`}>
